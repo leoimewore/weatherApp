419193a096690b6b4beb75f64d316a2ecbc1e24e
Leonard Isaac <leonardisaac@MacBook-Pro.local>
2022-11-06 15:42:12 -0800

    Updates

---
+++ precache-manifest.25926e797b35011bd135b8979e569fa3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c730fd0b85e9ccb47b70086665e218f",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "52b4aec1781339ca3d3f",
+    "url": "/weatherApp/static/css/main.5955f56d.chunk.css"
+  },
+  {
+    "revision": "5af5ef6396afe34a3696",
+    "url": "/weatherApp/static/js/2.937f5a68.chunk.js"
+  },
+  {
+    "revision": "7084ac9faa158a0408ffc277f29beab9",
+    "url": "/weatherApp/static/js/2.937f5a68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3d73ec59e64ea91dce1",
+    "url": "/weatherApp/static/js/3.90e6141f.chunk.js"
+  },
+  {
+    "revision": "52b4aec1781339ca3d3f",
+    "url": "/weatherApp/static/js/main.bc761321.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/weatherApp/static/js/main.bc761321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7da203d769fd473db9e",
+    "url": "/weatherApp/static/js/runtime-main.bca436fd.js"
+  }
+]);
